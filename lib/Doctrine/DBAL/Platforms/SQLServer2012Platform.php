988199f77219ff72df1df9270630d4a202d691c9
stchr <proverbs3V5-6>
2016-06-29 08:58:00 +0200

    Fix Modify Limit Query with newline before ORDER BY

--- lib/Doctrine/DBAL/Platforms/SQLServer2012Platform.php
+++ lib/Doctrine/DBAL/Platforms/SQLServer2012Platform.php
@@ -122,7 +122,7 @@ class SQLServer2012Platform extends SQLServer2008Platform
         if ($matchesCount > 0) {
             $orderByPos = $matches[0][($matchesCount - 1)][1];
         }
-        
+
         if ($orderByPos === false
             || substr_count($query, "(", $orderByPos) - substr_count($query, ")", $orderByPos)
         ) {
